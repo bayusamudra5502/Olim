367c8a3ada01897ca7c4bf31408c2343cb75ed4e
Bayu Samudra <bayusamudra.55.02.com@gmail.com>
2019-07-18 09:42:39 +0700

    Memulai Lembaran Baru

---
+++ STL/eksperimen/cin.tie.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string tes;
+    int a, b;
+    cout << "a : ";
+    getline(cin, tes);
+    cout << tes << endl;
+}
