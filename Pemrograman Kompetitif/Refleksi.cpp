4184c9cde3bc4d6b8a495c8082a0849804516279
Bayu Samudra <bayusamudra.55.02.com@gmail.com>
2019-07-28 09:46:17 +0700

    INIT

---
+++ Pemrograman Kompetitif/Refleksi.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr1[80][80];
+int arr2[80][80];
+int R, C;
+
+int main(int n, char **args)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
